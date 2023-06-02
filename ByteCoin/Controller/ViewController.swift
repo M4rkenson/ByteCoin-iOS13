541c75c8b968c5b743d324fe036fbaf778856fd8
Sergey Dolgorukov <dolgorukovs11629@gmail.com>
2023-06-02 14:47:02 +0300

    Added API Request Functionality

--- ByteCoin/Controller/ViewController.swift
+++ ByteCoin/Controller/ViewController.swift
@@ -24,7 +24,6 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
-    
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return coinManager.currencyArray.count
     }
